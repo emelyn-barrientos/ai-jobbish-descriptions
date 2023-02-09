d0bdb7f016d3c09afe3b5e388195f3f5515355dd
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-02-09 16:34:05 +1300

    Add form tag and Job Title label

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,17 @@
 import React, { useState } from 'react'
 
 export default function Dashboard() {
-  return <></>
+  return (
+    <>
+      <div>
+        <form>
+          <div>
+            <label className="sr-only" htmlFor="jobTitle">
+              Job Title
+            </label>
+          </div>
+        </form>
+      </div>
+    </>
+  )
 }
