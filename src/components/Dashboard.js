bd61a7ac9fc8a6f6fdb5678a8b3b8af3d0daa2d1
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-02-10 12:03:23 +1300

    Add styling to industry form field

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -32,6 +32,7 @@ export default function Dashboard() {
             </label>
             <input
               type="text"
+              className="block w-full rounded-md bg-white border border-gray-400 shadow-sm focus:border-purple-500 focus:ring-purple-500 sm:text-sm px-4 py-2 placeholder-gray-500 my-2 text-gray-900"
               name="industry"
               placeholder="Industry"
               id="industry"
