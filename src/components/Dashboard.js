56134dd74807ac007bfcf5c63de98fe884b8f208
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-02-10 13:27:12 +1300

    Add focus styling to textarea

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -50,7 +50,7 @@ export default function Dashboard() {
             </label>
             <textarea
               rows={7}
-              className="block w-full rounded-md bg-white border border-gray-400 shadow-sm"
+              className="block w-full rounded-md bg-white border border-gray-400 shadow-sm focus:border-purple-500 focus:ring-purple-500 sm:text-sm px-4 py-2 placeholder-gray-500 my-2 text-gray-900"
               name="keywords"
               placeholder="Keywords for AI"
               id="keywords"
