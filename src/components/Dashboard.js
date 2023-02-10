5a4532bdc1be165850f71a4ba9e937f0153f4a55
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-02-10 15:36:21 +1300

    Add styling for word count field

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -89,6 +89,7 @@ export default function Dashboard() {
             </label>
             <input
               type="number"
+              className="block w-full rounded-md bg-white border border-gray-400 shadow-sm focus:border-purple-500 focus:ring-purple-500 sm:text-sm px-4 py-2 placeholder-gray-500 my-2 text-gray-900"
               name="wordCount"
               placeholder="Word Count - Default 200 (Optional)"
               id="wordCount"
