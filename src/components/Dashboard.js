5d07c3b6de6fcb9229dc22b0f9fabd5624115388
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-02-13 14:05:28 +1300

    Add cursor pointer styling

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -105,7 +105,7 @@ export default function Dashboard() {
             />
             <button
               type="submit"
-              className="bg-purple-500 w-full hover:bg-purple-700 text-white font-bold mt-6 py-2 px-4 rounded"
+              className="bg-purple-500 w-full hover:bg-purple-700 cursor-pointer text-white font-bold mt-6 py-2 px-4 rounded"
               disabled={
                 isGenerating ||
                 jobTitle === '' ||
