a416e89c78b486ef603397a634dae8fe79e9cdd8
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-02-13 14:52:30 +1300

    Add styling to textarea

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -124,6 +124,11 @@ export default function Dashboard() {
           <label className="sr-only" htmlFor="output">
             Output
           </label>
+          <textarea
+            className="block w-full rounded-md bg-white border border-gray-400 shadow-sm focus:border-purple-500 focus:ring-purple-500 sm:text-sm px-4 py-2 placeholder-gray-500 my-2 text-gray-900"
+            name="output"
+            placeholder="AI Generated Job Description"
+          />
         </div>
       </div>
     </div>
