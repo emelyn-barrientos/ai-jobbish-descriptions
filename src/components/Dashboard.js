b010d18f25866dc22c8f0c3546e65527ce22e622
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-02-13 15:06:01 +1300

    Add ternary operator for length of textarea rows

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -127,6 +127,11 @@ export default function Dashboard() {
             Output
           </label>
           <textarea
+            rows={
+              jobDescription === ''
+                ? 7
+                : jobDescription.split('\\n').length + 12
+            }
             className="block w-full rounded-md bg-white border border-gray-400 shadow-sm focus:border-purple-500 focus:ring-purple-500 sm:text-sm px-4 py-2 placeholder-gray-500 my-2 text-gray-900"
             name="output"
             placeholder="AI Generated Job Description"
