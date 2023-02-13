2dffd87f089f222a681c93a7597b3731113f7b8f
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-02-13 15:21:17 +1300

    Add button styling

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -142,7 +142,11 @@ export default function Dashboard() {
             onChange={handleJobDescription}
             disabled={jobDescription === ''}
           />
-          <button type="submit" disabled={jobDescription === ''}>
+          <button
+            type="submit"
+            className="bg-purple-500 w-full hover:bg-purple-700 cursor-pointer text-white font-bold mt-6 py-2 px-4 rounded"
+            disabled={jobDescription === ''}
+          >
             {isCopied ? 'Copied!' : 'Copy to Clipboard'}
           </button>
         </div>
