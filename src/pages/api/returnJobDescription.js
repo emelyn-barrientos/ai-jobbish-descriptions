3a031d53c64b38151036ef744c8fb9d228a93171
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-02-15 09:59:43 +1300

    Add post method and API authorization

--- src/pages/api/returnJobDescription.js
+++ src/pages/api/returnJobDescription.js
@@ -1,6 +1,21 @@
-const generteDescription = asynch({jobTitle, industry, keyWords, tone, wordCount}) => {
-  try {
+const openAiUrl =
+  'https://api.openai.com/v1/engines/text-davinci-003/completions'
 
+const generteDescription = async ({
+  jobTitle,
+  industry,
+  keyWords,
+  tone,
+  wordCount,
+}) => {
+  try {
+    const response = await fetch(openAiUrl, {
+      method: 'POST',
+      headers: {
+        'Content Type': 'applications/json',
+        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+      },
+    })
   } catch (err) {
     console.error(err)
   }
