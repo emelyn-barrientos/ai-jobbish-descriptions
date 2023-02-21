597e6dc0b79fdf494fd2b169f3e1df08a4668b83
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-02-21 16:57:44 +1300

    Fix formatting

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -169,6 +169,7 @@ export default function Dashboard() {
             onChange={handleJobDescription}
             disabled={jobDescription === ''}
           />
+          {/* Copy Button */}
           <button
             type="submit"
             className="bg-purple-500 w-full hover:bg-purple-700 cursor-pointer text-white font-bold mt-6 py-2 px-4 rounded"
