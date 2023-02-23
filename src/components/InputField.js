d84e62bc6b24d987feddd8639733cb9f663c76e3
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-02-24 10:32:34 +1300

    Add input element and attributes

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -14,6 +14,16 @@ export default function InputField({
         <label className="sr-only" htmlFor={name}>
           {label}
         </label>
+        <input
+          type="text"
+          className="block w-full rounded-md bg-white border border-gray-400 shadow-sm focus:border-purple-500 focus:ring-purple-500 sm:text-sm px-4 py-2 placeholder-gray-500 my-2 text-gray-900"
+          name={name}
+          placeholder={placeholder}
+          id={name}
+          value={value}
+          onChange={onChange}
+          required={required}
+        />
       </div>
     </>
   )
