30a76d74aa3e4621783bdcc1cea9e660f6657567
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-02-24 11:01:23 +1300

    Import components and destructure props

---
+++ src/components/Form.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import InputField from './InputField'
+import TextareaField from './TextareaField'
+import SelectField from './SelectField'
+
+
+
+export default function Form(props) {
+  const { handleSubmit, handleJobTitle, handleIndustry, handleKeyWords, handleTone, handleWordCount, jobTitle, industry, keyWords, tone, wordCount, isGenerating } = props
+  return ()
+}
