c879d3f0afbd21b930194d2f17f364ffbd28d792
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-02-24 10:28:47 +1300

    Add label element

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -1,5 +1,20 @@
 import React, { useState } from 'react'
 
-export default function InputField({label, name, placeholder, value, onChange, required = false}) {
-  return ()
+export default function InputField({
+  label,
+  name,
+  placeholder,
+  value,
+  onChange,
+  required = false,
+}) {
+  return (
+    <>
+      <div className="flex flex-col">
+        <label className="sr-only" htmlFor={name}>
+          {label}
+        </label>
+      </div>
+    </>
+  )
 }
