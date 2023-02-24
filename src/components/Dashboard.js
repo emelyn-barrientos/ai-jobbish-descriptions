a721784c295fd0b618a532ac2b8dfa8d38b5f8c0
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-02-24 14:23:21 +1300

    Import form component and include props to pass down

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 
+import Form from './Form'
+
 export default function Dashboard() {
   const [jobDescription, setJobDescription] = useState('')
 
@@ -49,109 +51,26 @@ export default function Dashboard() {
   return (
     <div className="max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8 py-12">
       <div className="grid gap-y-12 md:grid-cols-2 md:gap-x-12">
-        <form onSubmit={handleSubmit}>
-          {/* Job Title */}
-          <div className="flex flex-col">
-            <label className="sr-only" htmlFor="jobTitle">
-              Job Title
-            </label>
-            <input
-              type="text"
-              className="block w-full rounded-md bg-white border border-gray-400 shadow-sm focus:border-purple-500 focus:ring-purple-500 sm:text-sm px-4 py-2 placeholder-gray-500 my-2 text-gray-900"
-              name="jobTitle"
-              placeholder="Job Title"
-              id="jobTitle"
-              value={jobTitle}
-              onChange={handleJobTitle}
-              required
-            />
-          </div>
-          {/* Industry */}
-          <div className="flex flex-col">
-            <label className="sr-only" htmlFor="industry">
-              Industry
-            </label>
-            <input
-              type="text"
-              className="block w-full rounded-md bg-white border border-gray-400 shadow-sm focus:border-purple-500 focus:ring-purple-500 sm:text-sm px-4 py-2 placeholder-gray-500 my-2 text-gray-900"
-              name="industry"
-              placeholder="Industry"
-              id="industry"
-              value={industry}
-              onChange={handleIndustry}
-              required
-            />
-          </div>
-          {/* AI Keywords */}
-          <div className="flex flex-col">
-            <label className="sr-only" htmlFor="keywords">
-              Keywords for AI
-            </label>
-            <textarea
-              rows={7}
-              className="block w-full rounded-md bg-white border border-gray-400 shadow-sm focus:border-purple-500 focus:ring-purple-500 sm:text-sm px-4 py-2 placeholder-gray-500 my-2 text-gray-900"
-              name="keywords"
-              placeholder="Keywords for AI"
-              id="keywords"
-              value={keyWords}
-              onChange={handleKeyWords}
-              required
-            />
-          </div>
-          {/* Tone */}
-          <div className="flex flex-col">
-            <label className="sr-only" htmlFor="tone">
-              Tone
-            </label>
-            <select
-              className="block w-full rounded-md bg-white border border-gray-400 shadow-sm focus:border-purple-500 focus:ring-purple-500 sm:text-sm px-4 py-2 placeholder-gray-500 my-2 text-gray-900"
-              name="tone"
-              id="tone"
-              value={tone}
-              onChange={handleTone}
-              required
-            >
-              <option value="default">Select Tone</option>
-              <option value="casual">Casual</option>
-              <option value="formal">Formal</option>
-              <option value="professional">Professional</option>
-              <option value="friendly">Friendly</option>
-            </select>
-          </div>
-          {/* Word Count */}
-          <div className="flex flex-col">
-            <label className="sr-only" htmlFor="wordCount">
-              Word Count (Optional)
-            </label>
-            <input
-              type="number"
-              className="block w-full rounded-md bg-white border border-gray-400 shadow-sm focus:border-purple-500 focus:ring-purple-500 sm:text-sm px-4 py-2 placeholder-gray-500 my-2 text-gray-900"
-              name="wordCount"
-              placeholder="Word Count - Default 200 (Optional)"
-              min={0}
-              max={800}
-              id="wordCount"
-              value={wordCount}
-              onChange={handleWordCount}
-            />
-            {/* Generate Job Description */}
-            <button
-              type="submit"
-              className="bg-purple-500 w-full hover:bg-purple-700 cursor-pointer text-white font-bold mt-6 py-2 px-4 rounded"
-              disabled={
-                isGenerating ||
-                jobTitle === '' ||
-                industry === '' ||
-                keyWords === '' ||
-                tone === ''
-              }
-            >
-              {isGenerating ? 'Generating...' : 'Generate Job Description'}
-            </button>
-          </div>
-        </form>
+        <Form
+          onSubmit={handleSubmit}
+          onJobTitleChange={handleJobTitle}
+          onIndustryChange={handleIndustry}
+          onKeyWordsChange={handleKeyWords}
+          onToneChange={handleTone}
+          onWordCountChange={handleWordCount}
+          onJobDescriptionChange={handleJobDescription}
+          jobTitle={jobTitle}
+          industry={industry}
+          keyWords={keyWords}
+          tone={tone}
+          wordCount={wordCount}
+          isGenerating={isGenerating}
+          isCopied={isCopied}
+          onCopy={handleCopy}
+          jobDescription={jobDescription}
+        />
         {/* Job Description Output */}
-        <div className="flex flex-col">
+        {/* <div className="flex flex-col">
           <label className="sr-only" htmlFor="output">
             Output
           </label>
@@ -170,7 +89,7 @@ export default function Dashboard() {
             disabled={jobDescription === ''}
           />
           {/* Copy Button */}
-          <button
+        {/* <button
             type="submit"
             className="bg-purple-500 w-full hover:bg-purple-700 cursor-pointer text-white font-bold mt-6 py-2 px-4 rounded"
             onClick={handleCopy}
@@ -178,7 +97,7 @@ export default function Dashboard() {
           >
             {isCopied ? 'Copied!' : 'Copy to Clipboard'}
           </button>
-        </div>
+        </div> */}
       </div>
     </div>
   )
