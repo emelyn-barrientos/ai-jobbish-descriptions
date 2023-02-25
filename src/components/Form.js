1f36c2c6209d3455e894084cfee37d7c576f998c
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-02-25 22:14:34 +1300

    Change the name of Button comp to SubmitButton

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,7 +2,7 @@ import React from 'react'
 import InputField from './InputField'
 import TextareaField from './TextareaField'
 import SelectField from './SelectField'
-import Button from './Button'
+import SubmitButton from './SubmitButton'
 
 export default function Form(props) {
   const {
@@ -79,7 +79,7 @@ export default function Form(props) {
           onChange={handleWordCount}
         />
         {/* Generate Job Description */}
-        <Button
+        <SubmitButton
           type="submit"
           className="bg-purple-500 w-full hover:bg-purple-700 cursor-pointer text-white font-bold mt-6 py-2 px-4 rounded"
           disabled={
