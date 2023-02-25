c468099998238fdd77cd0b9c38f7f853781e5e1f
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-02-25 21:51:22 +1300

    Add button element with attributes

--- src/components/CopyButton.js
+++ src/components/CopyButton.js
@@ -1,13 +1,21 @@
-import React from "react"
+import React from 'react'
 
-export default function CopyButton () {
-
-  const [isCopied, setIsCopied] = useState(false) 
+export default function CopyButton() {
+  const [isCopied, setIsCopied] = useState(false)
 
   const handleCopy = () => {
     navigator.clipboard.writeText(jobDescription)
     setIsCopied(true)
   }
 
-  return ()
+  return (
+    <>
+      <button
+        type="button"
+        className="bg-purple-500 w-full hover:bg-purple-700 cursor-pointer text-white font-bold mt-6 py-2 px-4 rounded"
+        onClick={}
+        disabled={}
+      ></button>
+    </>
+  )
 }
