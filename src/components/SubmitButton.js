f00c94993352f4941c33fbc9ba8af2b75c54fa6a
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-02-25 22:24:57 +1300

    Destructure props passed down from Form

--- src/components/SubmitButton.js
+++ src/components/SubmitButton.js
@@ -1,15 +1,27 @@
 import React from 'react'
 
-export default function Button(props) {
+export default function SubmitButton({
+  jobTitle,
+  industry,
+  keyWords,
+  tone,
+  wordCount,
+  isGenerating,
+}) {
   return (
     <>
       <button
-        type={props.type}
-        className={props.className}
-        onClick={props.onClick}
-        disabled={props.disabled}
+        type="submit"
+        className="bg-purple-500 w-full hover:bg-purple-700 cursor-pointer text-white font-bold mt-6 py-2 px-4 rounded"
+        disabled={
+          isGenerating ||
+          jobTitle === '' ||
+          industry === '' ||
+          keyWords === '' ||
+          tone === ''
+        }
       >
-        {props.label}
+        {isGenerating ? 'Generating...' : 'Generate Job Description'}
       </button>
     </>
   )
