d8cb32833fa90183480ee694606a495be30729be
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-02-27 14:16:50 +1300

    Change background, border, and focus color

--- src/components/JobDescriptionOutput.js
+++ src/components/JobDescriptionOutput.js
@@ -13,7 +13,7 @@ export default function JobDescriptionOutput({ jobDescription }) {
           rows={
             jobDescription === '' ? 7 : jobDescription.split('\\n').length + 12
           }
-          className="block w-full rounded-md bg-white border border-gray-400 shadow-sm focus:border-purple-500 focus:ring-purple-500 sm:text-sm px-4 py-2 placeholder-gray-500 my-2 text-gray-900"
+          className="block w-full rounded-md bg-stone-100 border-2 border-accent-aero focus:outline-none focus:border-accent-pink sm:text-sm px-4 py-2 placeholder-gray-500 my-2 text-gray-900"
           name="output"
           placeholder="AI Generated Job Description"
           id="output"
