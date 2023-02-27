87b36e60809b83d643a041b8ede91cd7c915ccc9
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-02-27 14:24:00 +1300

    Remove animation and change backgroun color

--- src/components/SubmitButton.js
+++ src/components/SubmitButton.js
@@ -12,7 +12,7 @@ export default function SubmitButton({
     <>
       <button
         type="submit"
-        className="bg-purple-500 w-full hover:bg-purple-700 cursor-pointer text-white font-bold mt-6 py-2 px-4 rounded animate"
+        className="bg-accent-aero w-full cursor-pointer text-white font-bold mt-6 py-2 px-4 rounded"
         disabled={
           isGenerating ||
           jobTitle === '' ||
