d99abad9bd4f4ff28fb7a194f19ef3bc15182ba2
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-02-28 19:56:25 +1300

    Change some of the border, text, and color styling

--- src/components/SubmitButton.js
+++ src/components/SubmitButton.js
@@ -12,7 +12,7 @@ export default function SubmitButton({
     <>
       <button
         type="submit"
-        className="btn relative inline-block font-medium group"
+        className="btn relative w-full rou mt-6 py-2 px-4 inline-block font-medium group"
         disabled={
           isGenerating ||
           jobTitle === '' ||
@@ -22,12 +22,18 @@ export default function SubmitButton({
           wordCount === ''
         }
       >
-        <span className="absolute inset-0 w-full h-full transition duration-400 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
-        <span className="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-red-300"></span>
-        <span className="relative text-black group-hover:text-white">
+        <span className="absolute bg-black rounded inset-0 w-full h-full transition duration-400 ease-out transform translate-x-1 translate-y-1 group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+        <span className="absolute inset-0 w-full h-full bg-accent-aero border-2 rounded border-black group-hover:bg-accent-pink group-hover:ease-in-out duration-100"></span>
+        <span className="relative text-black cursor-pointer font-bold mt-6 py-2 px-4 rounded group-hover:text-stone-100">
           {isGenerating ? 'Generating...' : 'Generate Job Description'}
         </span>
       </button>
     </>
   )
+}
+
+{
+  /* <button
+        type="button"
+        className="bg-purple-500 w-full hover:bg-purple-700 cursor-pointer text-white font-bold mt-6 py-2 px-4 rounded" */
 }
