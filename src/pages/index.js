6c96519b154625f1c2b3a6f45e15355dc5aa9984
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-02-28 21:00:21 +1300

    Adjust heading size for breakpoints

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
       </Head>
       <main className="bg-oxford-blue pt-10 pb-10 pr-5 pl-5 min-h-screen ">
         <div className="flex flex-col items-center justify-center px-4 py-2">
-          <h1 className="text-4xl md:text-6xl  font-sans font-bold text-stone-100">
+          <h1 className="text-4xl md:text-6xl sm:text-8xl font-sans font-bold text-stone-100">
             Job<span className="text-accent-aero">-</span>ish
             <span className="text-4xl md:text-6xl font-bold text-accent-aero">
               .
