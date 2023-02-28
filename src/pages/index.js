c2516aabf7a52a76abdfbce56aa9d6734677b5d2
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-02-28 20:12:17 +1300

    Change content text

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,15 +16,17 @@ export default function Home() {
       <main className={'bg-oxford-blue min-h-screen '}>
         <div className="flex flex-col items-center justify-center px-4 py-2">
           <h1 className="text-4xl md:text-6xl font-bold text-stone-100">
-            Jobbish Descriptions
+            Job<span className="text-accent-aero">-</span>ish
             <span className="text-4xl md:text-6xl font-bold text-accent-aero">
               .
             </span>
           </h1>
           <p className="mt-3 text-2xl text-stone-100">
-            Writing {''}{' '}
-            <span className=" text-accent-pink font-bold">Job Ads</span>
-            {''} Has Never Been Easier
+            Writing{' '}
+            <span className=" text-accent-pink font-bold">
+              Job Descriptions
+            </span>{' '}
+            Has Never Been Easier
           </p>
         </div>
         <Dashboard />
