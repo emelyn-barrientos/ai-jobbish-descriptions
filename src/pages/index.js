7668773ad1edbd23924c5a7ccffcd03e439c16ff
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-02-28 20:27:33 +1300

    Center <p> text for media queries

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Jobbish</title>
+        <title>Job-ish</title>
         <meta name="description" content="AI Job Description Generator" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
@@ -21,7 +21,7 @@ export default function Home() {
               .
             </span>
           </h1>
-          <p className="mt-3 text-2xl text-stone-100">
+          <p className="mt-3 text-2xl text-stone-100 text-center md:mx-auto">
             Writing{' '}
             <span className=" text-accent-pink font-bold">
               Job Descriptions
