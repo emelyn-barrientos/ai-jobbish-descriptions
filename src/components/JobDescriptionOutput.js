6a100614bfbf12a69860acd309d19e53925608e8
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-04-17 15:54:33 +1200

    Add useState import

--- src/components/JobDescriptionOutput.js
+++ src/components/JobDescriptionOutput.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import CopyButton from './CopyButton'
 
 export default function JobDescriptionOutput({ jobDescription }) {
