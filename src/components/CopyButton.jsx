f0658904080e0cd94eab57d5f5aefa3eef195c24
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-04-17 22:54:46 +1200

    Remove unnecessary React imports

--- src/components/CopyButton.jsx
+++ src/components/CopyButton.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 
 export default function CopyButton({ jobDescription }) {
   const [isCopied, setIsCopied] = useState(false)
