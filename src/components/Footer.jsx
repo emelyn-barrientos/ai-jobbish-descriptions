d3e02534fba0fba3cf4c881d430854085b4b93b7
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2023-04-18 15:17:40 +1200

    Initial styling and text for footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,22 @@
 export default function Footer() {
-  return <p>Footer here.</p>
+  return (
+    <>
+      <div className="text-center flex justify-center mb-4">
+        <h6 className="text-xs mb-1 sm:text-center">
+          Built with Next.js & Tailwind CSS
+        </h6>
+        <h6 className="text-xs mb-1 sm:text-center">
+          Illustrations by the talented{' '}
+          <a
+            className="underline underline-offset-2 decoration-1 transition ease-in-out hover:text-accent-green hover:duration-75"
+            href="https://rubyash.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Ruby Ash
+          </a>
+        </h6>
+      </div>
+    </>
+  )
 }
